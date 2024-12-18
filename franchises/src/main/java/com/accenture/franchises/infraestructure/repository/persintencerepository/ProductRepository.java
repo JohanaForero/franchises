06cd2cde054d1efdd08bfee3dc5e0b203f2c0a93
JohanaForero <johanaforeromendoza31@gmail.com>
2024-12-18 02:30:02 +0100

    "H6-GetProduct"

--- franchises/src/main/java/com/accenture/franchises/infraestructure/repository/persintencerepository/ProductRepository.java
+++ franchises/src/main/java/com/accenture/franchises/infraestructure/repository/persintencerepository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.accenture.franchises.infraestructure.repository.persintencerepository;
 
 import com.accenture.franchises.infraestructure.repository.entity.ProductEntity;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -10,4 +12,7 @@ public interface ProductRepository extends ReactiveMongoRepository<ProductEntity
     Mono<Long> deleteByName(String nameProduct);
 
     Mono<ProductEntity> findByName(String nameProduct);
+
+    @Aggregation(pipeline = {"{ $sort: { total: -1 } }", "{ $limit: 1 }"})
+    Flux<ProductEntity> findProductWithMaxTotal();
 }
